c13698fd508e94c8fec33a13b945b15de983a83c
Aleksey Timin <atimin@gmail.com>
2021-06-07 20:02:30 +0500

    fix pylint

--- app/config.py
+++ app/config.py
@@ -4,10 +4,13 @@ from pydantic import BaseSettings, Field
 
 class Config(BaseSettings):
     """App configuration"""
-    mongodb_url: str = Field(description='MongoDB URL with credentials e.g. mongodb:user:pwd@server:port',
-                             default='mongodb://root:root@mongo:27017')
+    mongodb_url: str = Field(
+        description='MongoDB URL with credentials e.g. mongodb:user:pwd@server:port',
+        default='mongodb://root:root@mongo:27017')
+
     salt: str = Field(description='Salt for hashing passwords', default='salty')
-    authjwt_secret_key: str = Field(description='Secret key for JWT', alias='secret_key', default='secret')
+    authjwt_secret_key: str = Field(description='Secret key for JWT',
+                                    alias='secret_key', default='secret')
 
 
 CONFIG = Config()
